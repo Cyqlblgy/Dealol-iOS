47d83ad623ca092e040417d75a1c4900c324d257
Bin Lang <blang@roamdata.com>
2017-05-22 18:55:22 -0400

    Initial commit

---
+++ Dealol/Dealol/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Dealol
+//
+//  Created by Bin Lang on 5/22/17.
+//  Copyright © 2017 Dealol. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    @IBOutlet weak var startButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
