8c7abc0673c03be8cbc121a4667eb1ea721739e2
Langbin <langbin0001@gmail.com>
2018-04-01 15:21:56 -0700

    Swipe to delete function added

--- Dealol/Dealol/SearchResultViewController.swift
+++ Dealol/Dealol/SearchResultViewController.swift
@@ -177,6 +177,18 @@ extension SearchResultViewController: UITableViewDelegate{
             self.navigationController?.pushViewController(bvc, animated: true)
         }
     }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            totalResult.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            maxNumber -= 1
+        }
+    }
 }
 
 extension SearchResultViewController: UITableViewDataSource{
